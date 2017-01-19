c1b6b9c54ccb03b83dc44f71eb9091f57e1fa57c
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2017-01-19 18:52:35 -0500

    code along progress point

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import $ from 'jquery';
 import './App.css';
 
+// need unique key to help react find stuff faster. 
+
+function Houses({
+  houses,
+}) {
+  return (
+    <div>
+      {houses.map(house => <div key={house.name}>{house.name}</div>)}
+    </div>
+  )
+}
+
+function House ({
+  name,
+}) {
+  return <div key={name}>{name}</div>
+}
+
 class App extends Component {
+  constructor() { // used to define private variables just for that class you're using
+    super(); // used to call a function's parents. if we want to have access to component's state by extending it, we need it. 
+    this.state = {
+      houses: [],
+    }
+
+  }
+
+  componentDidMount() {
+    $.get('http://www.anapioficeandfire.com/api/houses').then(response => {
+      console.log(response);
+      this.setState({
+        houses: response,
+      })
+    })
+  }
+
   render() {
+
     return (
       <div className="App">
-        Hello World
+        <Houses  houses={this.state.houses}/> 
       </div>
     );
   }
 }
 
 export default App;
+
+// Exports are taking whatever's in that file to pull it into another file.
+// If you default export something, when you import it, you can name it whatever you want.
+// If you did a named export (without default), you have to use the same name when you import.
